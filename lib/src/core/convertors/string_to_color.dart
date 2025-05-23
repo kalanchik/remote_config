d165cf8994f12d3339dc6b19252c22b03ca16a55
kalanchik <kalanchik123@gmail.com>
2025-05-24 02:00:07 +0300

    add color to string

--- lib/src/core/convertors/string_to_color.dart
+++ lib/src/core/convertors/string_to_color.dart
@@ -10,5 +10,17 @@ class StringToColorConverter implements JsonConverter<Color, String> {
   Color fromJson(String json) => ModuleUtils.hexToColor(json);
 
   @override
-  String toJson(Color object) => object.toString();
+  String toJson(Color object) => colorToHex(object.r, object.g, object.b);
+
+  String colorToHex(double red, double green, double blue) {
+    int r = (red.clamp(0.0, 1.0) * 255).round();
+    int g = (green.clamp(0.0, 1.0) * 255).round();
+    int b = (blue.clamp(0.0, 1.0) * 255).round();
+
+    return '#'
+            '${r.toRadixString(16).padLeft(2, '0')}'
+            '${g.toRadixString(16).padLeft(2, '0')}'
+            '${b.toRadixString(16).padLeft(2, '0')}'
+        .toUpperCase();
+  }
 }
