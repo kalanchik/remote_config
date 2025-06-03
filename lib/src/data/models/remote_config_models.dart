6beea1b34c3b6010a8af09762bd88b47e90f2ab0
kalanchik <kalanchik123@gmail.com>
2025-06-04 00:23:20 +0300

    fix

--- lib/src/data/models/remote_config_models.dart
+++ lib/src/data/models/remote_config_models.dart
@@ -4,3 +4,4 @@ export '../models/banks_info/bank_info_dto.dart';
 export '../models/change_log/config_change_log_dto.dart';
 export '../models/merchants/merchant_info_dto.dart';
 export '../models/tokens_info/config_tokens_dto.dart';
+export '../models/tokens_info/token_info_dto.dart';
