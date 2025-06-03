f71de25cb3bfad4a9ab57cc5ad818d4fd5411bfe
kalanchik <kalanchik123@gmail.com>
2025-06-03 13:53:39 +0300

    fix

--- lib/src/domain/entitty/entity.dart
+++ lib/src/domain/entitty/entity.dart
@@ -1,1 +1,3 @@
 export '../entitty/available_banks/available_bank_form.dart';
+export '../entitty/available_banks/main_address_type.dart';
+export '../entitty/available_banks/secondary_address_type.dart';
